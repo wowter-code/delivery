d7403ee6ac3773dbb324195a9347cb1c566da6e0
wowter-code <ionut.horia.as@gmail.com>
2026-02-24 21:08:29 +0200

    Update x.js

--- x.js
+++ x.js
@@ -1,4 +1,1 @@
-document.body.insertAdjacentHTML(
-  'beforeend',
-  '<div style="position:fixed;top:0;left:0;background:red;color:white;padding:10px;z-index:9999">XSS Executed</div>'
-);
+window.location = "https://www.youtube.com/";
